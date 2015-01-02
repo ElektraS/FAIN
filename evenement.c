b2bd911a244cf00d5e095eb1ac2832a1e89243e6
Laetitia <letoffe.laetitia@gmail.com>
2015-01-02 13:15:37 +0100

    nouveau sommet

--- evenement.c
+++ evenement.c
@@ -1,1 +1,24 @@
 #include "evenement.h"
+
+
+listc nouveauSommet(listc l, int x, int y)
+{
+	element * newElement = malloc(sizeof(element));
+	newElement->x=x;
+	newElement->y=y;
+
+	newElement->next = l;
+
+	return newElement;
+}
+
+
+void mouseClick(int button, int state, int x, int y, listc l)
+{
+	if(state==GLUT_DOWN && button==GLUT_LEFT_BUTTON)
+	{
+		fprintf(stderr,"Presser ");
+		nouveauSommet(l,x,y);
+		glutPostRedisplay();
+	}
+}

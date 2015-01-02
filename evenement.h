cda5b3483944eff0f52174881508aa8e681cbf6e
PALLAS <pallas.aurelie@gmail.com>
2015-01-02 13:06:46 +0100

    add type list

--- evenement.h
+++ evenement.h
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/gl.h>
+#include <stdlib.h>
+ 
+typedef struct element element;
+struct element
+{
+    int x;
+    int y;
+    struct element *next;
+};
+ 
+typedef element* listc;

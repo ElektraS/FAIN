8b486e3e11988268da18cde81842e6577159428e
Laetitia <letoffe.laetitia@gmail.com>
2015-01-05 16:58:23 +0100

    fonctions evenements.h

--- evenement.h
+++ evenement.h
@@ -12,3 +12,11 @@ struct element
 };
  
 typedef element* listc;
+
+listc nouveauSommet(listc l, int x, int y);
+
+void firstElement(listc l, int x, int y);
+
+void mouseClick(int button, int state, int x, int y, listc l);
+
+void keyboardC(unsigned char key, listc l);
